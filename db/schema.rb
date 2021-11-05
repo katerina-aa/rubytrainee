0e906c644643adc5fa39478c45c24e029155f3e8
Andrii Mishchuk <amishchu@cisco.com>
2021-11-05 13:56:32 +0200

    api setup2

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_162148) do
+ActiveRecord::Schema.define(version: 2021_11_05_115019) do
 
   create_table "users", force: :cascade do |t|
-    t.string "fname"
-    t.string "lname"
-    t.string "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "name"
   end
 
 end
